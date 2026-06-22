9b1d3932d14b99a8c19368153e3e082b43759f79
eduardovbrezende <eduardovb.rezende@gmail.com>
2026-06-22 19:48:42 -0300

    feat - Adiciona repository Estadio

---
+++ backend/src/main/java/com/pitscore/backend/repository/EstadioRepository.java
@@ -0,0 +1,9 @@
+package com.pitscore.backend.repository;
+
+import com.pitscore.backend.model.Estadio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadioRepository extends JpaRepository<Estadio, Long> {
+}
